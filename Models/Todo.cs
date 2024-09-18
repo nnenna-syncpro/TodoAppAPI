9ffba34975ca3626c4256059833a4d4bac95bfa5
esot0001 <esot0001@algonquinlive.com>
2024-09-18 06:10:56 -0400

    feat: adds todo model

---
+++ Models/Todo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoAppAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter a todo item")]
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage = "Please select a category")]
+        public bool IsCompleted { get; set; }
+        public string CategoryId { get; set; }
+        [ValidateNever]
+        public Category Category { get; set; }
+        [Required(ErrorMessage = "Please select a status")]
+        public string StatusId { get; set; }
+        [ValidateNever]
+        public Status Status { get; set; }
+        [Required(ErrorMessage = "Please select a priority level")]
+        public string PriorityId { get; set; }
+        [ValidateNever]
+        public Priority Priority { get; set; }
+        public bool Overdue => StatusId == "open" && DueDate < DateTime.Today;
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
