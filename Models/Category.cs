3614dee2e8436f490ecf4d887fd9c49fd7ee6676
esot0001 <esot0001@algonquinlive.com>
2024-09-18 05:32:48 -0400

    feat: adds models for category, status, and priority

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoAppAPI.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Models/Priority.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoAppAPI.Models
+{
+    public class Priority
+    {
+        public int PriorityId { get; set; }
+        public string Title { get; set; }
+    }
+}
